44e4c5459505ef50dc4573fe59e19da0eeb4c415
James M. Allen <james.m.allen@gmail.com>
2016-06-08 11:03:19 -0400

    First pass at ajax updates for "done" status

---
+++ ToDoApp/Scripts/Tasks.js
@@ -0,0 +1,28 @@
+﻿
+
+$(document).ready(function () {
+    $('.mark-done').click(function (event) {
+        console.log(event);
+        var form = event.currentTarget.form;
+        var taskDoneNode = form.TaskDone;
+        console.log(taskDoneNode.value);
+        console.log(form.action);
+        $.ajax({
+            url: form.action,
+            data: {
+                TaskDone: taskDoneNode.value
+            },
+            method: 'POST',
+            success: function () {
+                if (taskDoneNode.value == "True") {
+                    $(event.currentTarget).find('.glyphicon').addClass('glyphicon-check').removeClass('glyphicon-unchecked');
+                    taskDoneNode.value = 'False';
+                } else {
+                    $(event.currentTarget).find('.glyphicon').addClass('glyphicon-unchecked').removeClass('glyphicon-check');
+                    taskDoneNode.value = 'True';
+                }
+            }
+        });
+        return false;
+    });
+});
